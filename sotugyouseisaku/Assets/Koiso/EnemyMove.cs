df7097fadda83769bd550a49e2de5c1e2641a67b
koiso9758 <koiso9758@gmail.com>
2021-10-20 23:29:17 +0900

    ゾンビの基本動作

--- sotugyouseisaku/Assets/Koiso/EnemyMove.cs
+++ sotugyouseisaku/Assets/Koiso/EnemyMove.cs
@@ -5,81 +5,169 @@ using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
-    //巡回地点オブジェクトを格納する配列
-    public Transform[] points;
-    //巡回地点のオブジェクト数（初期値=0）
+    enum EnemyState
+    {
+        PATROL,
+        CHASE,
+        DAMAGE,
+        ATTACK
+    }
+    [SerializeField] private EnemyState state = EnemyState.PATROL;
+    [SerializeField] private int hp = 100;
+    [SerializeField] private int speed = 15;
+    //ダメ―ジエフェクト
+    [SerializeField] private GameObject bloodObj;
+    //攻撃の判定
+    [SerializeField] private GameObject attack;
+    //巡回地点オブジェクト
+    [SerializeField] public Transform[] points;
+    //巡回地点のオブジェクト数
     private int destPoint = 0;
-    //NavMesh Agent コンポーネントを格納する変数
     private NavMeshAgent agent;
-
     //プレイヤーの座標
-    [SerializeField] private Transform Target;
-    private Vector3 TargetPos;
+    [SerializeField] private Transform player;
+    private Vector3 playerPos;
     //発見距離
-    [SerializeField] int Distance = 50;
-    //Rayが当たったオブジェクトの情報を入れる箱
+    [SerializeField] int chaseDistance = 50;
+    //攻撃距離
+    [SerializeField] int attackDistance = 0;
     RaycastHit hit;
 
-    // ゲームスタート時の処理
+    bool isChase=true;
+    bool isAttack = true;
+    bool isLook = false;
+
     void Start()
     {
-        // 変数"agent"に NavMesh Agent コンポーネントを格納
+        attack.SetActive(false);
         agent = GetComponent<NavMeshAgent>();
-        // 巡回地点間の移動を継続させるために自動ブレーキを無効化
-        //（エージェントは目的地点に近づいても減速しない)
-        agent.autoBraking = false;
-        // 次の巡回地点の処理を実行
         GotoNextPoint();
+        agent.speed = speed;
     }
 
-    // 次の巡回地点を設定する処理
     void GotoNextPoint()
     {
-        // 巡回地点が設定されていなければ
-        if (points.Length == 0)
-            // 処理を返します
-            return;
-        // 現在選択されている配列の座標を巡回地点の座標に代入
+        if (points.Length == 0) return;
         agent.destination = points[destPoint].position;
-        // 配列の中から次の巡回地点を選択（必要に応じて繰り返し）
         destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            Damage();
+        }
+    }
+
+    void PlayerChase()
+    {
+        state = EnemyState.CHASE;
+        agent.destination = player.position;
+    }
+
+    void Patrol()
+    {
+        state = EnemyState.PATROL;
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+
+    void Damage()
+    {
+        state = EnemyState.DAMAGE;
+        if(hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            hp -= 5;
+            //血しぶきエフェクト
+            //Instantiate(bloodObj, this.transform.position, Quaternion.identity);            
+        }       
+        StartCoroutine("Colortimer", 0.1f);
+    }
+
+    //攻撃クールダウン
+    IEnumerator Attacktimer(int time)
+    {
+        state = EnemyState.ATTACK;
+        attack.SetActive(true);
+        //Material mat = this.GetComponent<Renderer>().material;
+        while (time >= 0)
+        {
+            agent.velocity = Vector3.zero;
+            agent.isStopped = true;
+            //mat.color = new Color(0.0f, 0.0f, 1.0f, 1.0f);
+            yield return new WaitForSeconds(1f);
+            Debug.Log(time);
+            --time;
+            attack.SetActive(false);
+        }
+        //mat.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        agent.isStopped = false;
+        isAttack = true;
+    }
+
+    //状態を表す点滅
+    IEnumerator Colortimer(int time)
+    {
+        //Material mat = this.GetComponent<Renderer>().material;
+        while (time >= 0)
+        {           
+            //mat.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+            yield return new WaitForSeconds(0.1f);
+            --time;
+        }
+        //mat.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
     }
 
     // ゲーム実行中の繰り返し処理
     void Update()
     {
-        // エージェントが現在の巡回地点に到達したら
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            // 次の巡回地点を設定する処理を実行
-            GotoNextPoint();
-
-        //向きをプレイヤーに変える
-        //transform.rotation =
-        //Quaternion.LookRotation(Target.position - transform.position);
-
-        //ターゲット座標 + 位置調整
-        TargetPos = (Target.position - transform.position) + new Vector3(0, 1, 0);
-        //rayの生成
-        Ray ray = new Ray(transform.position, TargetPos);
+        playerPos = (player.position - transform.position) + new Vector3(0, 1, 0);
+        Ray ray = new Ray(transform.position, playerPos);
         //デバッグ用
         Debug.DrawLine(ray.origin, hit.point, Color.red);
-        //当たり判定 
-        if (Physics.Raycast(ray, out hit, Distance))
+
+        ////アニメーション
+        //switch(EnemyState)
+        //{
+        //    case EnemyState.PATROL:
+        //        break;
+        //    case EnemyState.CHASE:
+        //        break;
+        //    case EnemyState.DAMAGE:
+        //        break;
+        //    case EnemyState.ATTACK:
+        //        break;
+        //}
+
+        if (Physics.Raycast(ray, out hit, chaseDistance))
         {
-            //プレイヤータグに当たっていたら
             if (hit.collider.tag == "Player")
             {
-                Debug.Log("RayがPlayerに当たった");
-                //プレイヤーを追いかける
-                agent.destination = Target.position;
+                if (Physics.Raycast(ray, out hit, attackDistance))
+                {
+                    if (isAttack)
+                    {
+                        StartCoroutine("Attacktimer", 1);
+                        isAttack = false;
+                    }
+                }
+                PlayerChase();
             }
-            //発見していない場合
-            else
-            {
-                
-            }
+            else Patrol();
         }
+        else Patrol();
 
-        Debug.Log("巡回地点は"+points[destPoint]);
+        //デバッグ用
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Damage();
+        }
     }
+
+
 }
