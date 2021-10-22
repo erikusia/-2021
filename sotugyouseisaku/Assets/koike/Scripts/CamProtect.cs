81fca35fbf3d39034e8bdbf22316f03c9855e607
kaede828 <kaede1999828@gmail.com>
2021-10-22 13:57:06 +0900

    弾飛ばない;_;

--- sotugyouseisaku/Assets/koike/Scripts/CamProtect.cs
+++ sotugyouseisaku/Assets/koike/Scripts/CamProtect.cs
@@ -33,17 +33,14 @@ public class CamProtect : MonoBehaviour
     {
         if (Physics.CheckSphere(gameObject.transform.position, 0.3f, Mask))
         {
-            Debug.Log("A");
-            transform.position = Vector3.Lerp(transform.position, gameObject.transform.position, 1);
+            transform.position = Vector3.Lerp(transform.position, gameObject.transform.position, 0.55f);
         }
         else if (Physics.SphereCast(gameObject.transform.position, 0.3f, (transform.position - gameObject.transform.position).normalized, out Hit, Distance, Mask))
         {
-            Debug.Log("B");
             transform.position = gameObject.transform.position + (transform.position - gameObject.transform.position).normalized * Hit.distance;
         }
         else
         {
-            Debug.Log("C");
             transform.localPosition = Vector3.Lerp(transform.localPosition, Position, 1);
 
         }
